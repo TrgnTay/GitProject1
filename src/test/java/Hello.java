aaf57e529241fab6e00a3b37dc405cd761ab9a3d
Turghan <ttaayy100@gmail.com>
2021-09-13 13:47:56 +0200

    added more hello feature

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developping hello feature");
+        System.out.println("Github repo is already publiched to GitHub");
+        System.out.println("doing more work");
     }
 }
